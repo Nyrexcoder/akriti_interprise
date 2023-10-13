e27f98faa9cdf5f79b897bab6feff861835fedc9
Sunny Kumar <114385508+SunnyWeb2022@users.noreply.github.com>
2023-10-13 20:33:06 +0530

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,9 +1,9 @@
 <?php 
-    // session_start();
-    // if(!isset($_SESSION['username']))
-    // {
-    //   header('location:../index.php'); 
-    // }
+    session_start();
+    if(!isset($_SESSION['user']))
+    {
+     header('location:./adminlogin.php');
+    }
 
         include('includes/header1.php');
         // include('includes/footer1.php');
